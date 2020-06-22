9c7c384023d089fac1c0da354b0290747bcfe0c0
Sean Floyd <seanfloyd@icloud.com>
2020-06-22 09:20:03 -0400

    Associate User with Event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :event_registrations
+  has_many :events, through: :event_registrations
+
   validates :username, :password, presence: true
   validates :username, uniqueness: { case_sensitive: false }
   validates :admin, inclusion: { in: [true, false] }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { described_class.create(username: 'first', password: 'supersecure') }
 
+  describe 'Associations' do
+    it { should have_many(:event_registrations) }
+    it { should have_many(:events).through(:event_registrations) }
+  end
+
   describe 'Validations' do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username).ignoring_case_sensitivity }
