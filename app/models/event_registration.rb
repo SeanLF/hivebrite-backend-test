79981da8a8ddfa9efa73c175fdb14906792db0c2
Sean Floyd <seanfloyd@icloud.com>
2020-06-25 14:18:13 -0400

    Validate required attributes on event registration

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -2,6 +2,25 @@ class EventRegistration < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :event_id, :user_id, presence: true
+  has_many :event_registration_attributes
+  has_many :event_registration_attribute_values
+
+  validates :event, :user, presence: true
   validates :user_id, uniqueness: { scope: :event_id }
+
+  validate :required_attributes
+
+  private
+
+  def required_attributes
+    errors[:base] << 'missing required attributes' unless all_required_attributes_present?
+  end
+
+  def all_required_attributes_present?
+    required_attribute_ids = event_registration_attributes.required.pluck(:id)
+    return true if required_attribute_ids.count.zero?
+
+    event_registration_attributes_value_ids = event_registration_attribute_values.pluck(:event_registration_attribute_id)
+    required_attribute_ids.intersection(event_registration_attributes_value_ids) == required_attribute_ids
+  end
 end
--- spec/models/event_registration_spec.rb
+++ spec/models/event_registration_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe EventRegistration, type: :model do
+  describe 'Associations' do
+    it { should have_many(:event_registration_attribute_values) }
+  end
+
   let(:event) { Event.create(name: 'Event') }
   let(:user) { User.create(username: '@me', password: 'supersecure') }
   subject { described_class.new(event_id: event.id, user_id: user.id) }
 
+  describe 'Associations' do
+    it { should belong_to(:event) }
+    it { should belong_to(:user) }
+
+    it { should have_many(:event_registration_attribute_values) }
+  end
+
   describe 'Validations' do
-    it { should validate_presence_of(:event_id) }
-    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:event) }
+    it { should validate_presence_of(:user) }
 
     it { should validate_uniqueness_of(:user_id).scoped_to(:event_id) }
 
